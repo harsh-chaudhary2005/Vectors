83aea076aafb7aec0963c69fb3a54f975dbd9d29
Harsh Chaudhary <hc492250@gmail.com>
2026-06-06 22:54:17 +0530

    Add files via upload

---
+++ Majority_element.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int majorityElement(vector<int>& nums){
+    int n = nums.size();
+    for(int val : nums){
+        int freq = 0;
+        for(int el : nums){
+            if (el == val)
+            {
+                freq++;
+            }
+            
+        }  if (freq > n/2)
+        {
+            return val;
+        }
+        
+    }
+    return -1;
+}
+int main(){
+  vector<int> nums = {1, 2, 2, 1, 2};
+
+  cout << "Majority Element is = "<< majorityElement(nums)<<endl;
+  return 0;
+}
